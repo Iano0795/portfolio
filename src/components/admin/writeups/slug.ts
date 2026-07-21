0f89fa3d892b6f9bf8e4612e2b395c4823c088f5
Iano0795 <iankipkorir668@gmail.com>
2026-07-21 19:50:09 +0300

    feat: add slug generation function for writeup titles

---
+++ src/components/admin/writeups/slug.ts
@@ -0,0 +1,7 @@
+export function generateSlugFromTitle(title: string): string {
+  return title
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+    .substring(0, 180);
+}
